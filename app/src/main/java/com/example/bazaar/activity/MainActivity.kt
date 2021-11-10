6d71125478465e395fea0cd0c371e0e37020fe7d
dkadrienn <deak.adrienn10@gmail.com>
2021-11-10 16:31:45 +0200

    Added navigation

--- app/src/main/java/com/example/bazaar/activity/MainActivity.kt
+++ app/src/main/java/com/example/bazaar/activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.bazaar.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bazaar.R
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/bazaar/fragment/LogInFragment.kt
+++ app/src/main/java/com/example/bazaar/fragment/LogInFragment.kt
@@ -1,60 +1,37 @@
 package com.example.bazaar.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.bazaar.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LogInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LogInFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val TAG = Class::class.java.simpleName
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LogInFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LogInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val view: View = inflater.inflate(R.layout.fragment_log_in, container, false)
+        val buttonSignUp = view.findViewById<Button>(R.id.buttonSignUp)
+        buttonSignUp.setOnClickListener {
+            Log.d(TAG, "button signed up clicked!")
+            Navigation.findNavController(view)
+                .navigate(R.id.action_logInFragment_to_registerFragment)
+        }
+        val textViewClickHere = view.findViewById<TextView>(R.id.textViewClickHere)
+        textViewClickHere.setOnClickListener {
+            Log.d(TAG, "Text Click Me clicked!")
+            Navigation.findNavController(view)
+                .navigate(R.id.action_logInFragment_to_forgetPasswordFragment)
+        }
+        return view
     }
 }
